72b990568ed1ae69e79d949d9c065c24e49d13cb
Jay-SCM <john.farmer1086@gmail.com>
2023-12-06 23:43:42 -0800

    added page and updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
-// Navbar.jsx
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/bitcoin-rates">Bitcoin Rates</Link>
-                </li>
-            </ul>
+        <nav className="navbar">
+            <Link to="/">
+                <img
+                    src="../assets/icon/SylvIcon.jpg"  // icon does not show on the page
+                    alt="Sylv Icon"
+                    className="icon-image"/>
+            </Link>
+            <div className="navbar-title">CofeeBAR</div>
+            <div className="navbar-links">
+                <Link to="/">Home</Link>
+                <Link to="/new-page">New Page</Link>
+                <Link to="/bitcoin-rates">Bitcoin Rates</Link>
+                <Link to="/login">Login</Link>
+            </div>
         </nav>);};
 export default Navbar;
+
 
